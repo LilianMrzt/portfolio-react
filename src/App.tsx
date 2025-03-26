8a0f870b0f837ffe052ed5fb65aa9011794066d5
Lilian Marzet <lilian.marzet123@gmail.com>
2025-03-26 11:32:43 +0100

    [FEAT] initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import { type ReactNode } from 'react'
+
+const App = (): ReactNode => {
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const root = ReactDOM.createRoot(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.getElementById('root')!
+)
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
