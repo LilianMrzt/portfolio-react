83e18d893e14d647cb07d35ba4d01aa0b9db3f6f
Lilian Marzet <lilian.marzet123@gmail.com>
2025-03-26 11:56:40 +0100

    [FEAT] Ajout de couleurs de theme

---
+++ src/constants/Theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+    primary: getComputedStyle(document.documentElement).getPropertyValue('--primary').trim(),
+    onPrimary: getComputedStyle(document.documentElement).getPropertyValue('--on-primary').trim(),
+    background: getComputedStyle(document.documentElement).getPropertyValue('--background').trim(),
+    onBackground: getComputedStyle(document.documentElement).getPropertyValue('--on-background').trim(),
+    onBackgroundSecondary: getComputedStyle(document.documentElement).getPropertyValue('--on-background-secondary').trim(),
+    surface: getComputedStyle(document.documentElement).getPropertyValue('--surface').trim(),
+    onSurface: getComputedStyle(document.documentElement).getPropertyValue('--on-surface').trim(),
+    secondaryText: getComputedStyle(document.documentElement).getPropertyValue('--secondary-text').trim(),
+    text: getComputedStyle(document.documentElement).getPropertyValue('--text').trim(),
+    outline: getComputedStyle(document.documentElement).getPropertyValue('--outline').trim()
+}
+
+export default theme
